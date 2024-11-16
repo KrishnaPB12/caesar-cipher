5a79e31491d4c30cbc49459087a85624a39fb941
Krishna Bhattarai <lifekpb@gmail.com>
2024-11-16 13:13:30 -0500

    complate challange caesar-ciper

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
-# add your code here
+input_text = input("Please Enter a Sentence- ")
+
+
+input_text = input_text.lower()
+
+
+encrypted_text = "" #An empty string
+shift_value = 5 #Shift value
+
+for char in input_text:
+
+  if('a'<= char <='z'):
+    new_character = chr((ord(char) - ord('a') + shift_value) % 26 + ord('a'))
+    encrypted_text=encrypted_text + new_character
+  else:
+    encrypted_text+=char
+
+
+print("The entered sentence is:\n",input_text)
+print("The encrypted sentence is:\n",encrypted_text)
